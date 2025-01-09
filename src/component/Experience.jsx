01db355f9bb8efaa5301931e8e0dcf5acc5d784d
difarifaldi <difarifaldi0906@gmail.com>
2025-01-09 17:07:04 +0700

    ubah animasi experience

--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -43,7 +43,7 @@ const Experience = () => {
             key={index}
             whileInView={{ opacity: 1 }} // Efek saat elemen terlihat di layar
             initial={{ opacity: 0 }} // Efek awal: tersembunyi
-            transition={{ duration: 0.6 }} // Durasi animasi dan delay berdasarkan urutan
+            transition={{ delay: index * 0.3, duration: 0.6 }} // Durasi animasi dan delay berdasarkan urutan
             viewport={{ once: false, amount: 0.25 }} // Triggers ketika elemen 25% terlihat di layar
           >
             <div className="relative flex py-6 text-white">
