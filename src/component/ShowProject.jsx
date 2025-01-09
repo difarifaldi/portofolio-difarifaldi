2227c07a62969af55edd78bb07c9286bce0b47f9
difarifaldi <difarifaldi0906@gmail.com>
2025-01-09 14:00:47 +0700

    addImageShowProject

--- src/component/ShowProject.jsx
+++ src/component/ShowProject.jsx
@@ -1,27 +1,47 @@
 import { useParams } from "react-router-dom"; // Import useParams untuk mengambil ID dari URL
 import PropTypes from "prop-types"; // Import PropTypes untuk validasi props
+import { useEffect } from "react";
 
 const ShowProject = ({ projects }) => {
   const { id } = useParams(); // Ambil ID proyek dari URL
   const project = projects.find((proj) => proj.id === parseInt(id)); // Mengonversi id menjadi angka
+  useEffect(() => {
+    // Menggulirkan halaman ke atas saat komponen Detail dimuat
+    window.scrollTo(0, 0);
+  }, []);
 
   if (!project) {
     return <div>Project not found!</div>; // Jika proyek tidak ditemukan
   }
 
   return (
-    <div className="bg-gray-800 p-6 pt-24">
-      <div className="max-w-6xl mx-auto">
-        <h1 className="text-white text-4xl font-extrabold">{project.title}</h1>
-        <p className="text-white mt-4">{project.description}</p>
-        <div className="mt-6">
-          <h2 className="text-white text-xl font-bold">Technologies Used:</h2>
-          <ul className="list-disc pl-5 text-white">
-            {project.technologies.map((tech, idx) => (
-              <li key={idx}>{tech}</li>
-            ))}
-          </ul>
+    <div className="bg-gray-800  pt-16 min-h-screen">
+      <div className="grid grid-cols-1 sm:grid-cols-2  max-w-7xl mx-auto mt-6">
+        <div className="p-8 ">
+          <h1 className="text-gray-500 text-2xl font-extrabold">PROJECT NAME</h1>
+          <h1 className="text-white text-3xl font-extrabold">{project.title}</h1>
+          <div className="mt-6">
+            <h1 className="text-gray-500 text-2xl font-extrabold">TECHNOLOGY</h1>
+            <p className="text-white text-2xl">{project.technologies.join(", ")}</p>
+          </div>
+          <div className="mt-6">
+            <h1 className="text-gray-500 text-2xl font-extrabold">YEAR</h1>
+            <p className="text-white text-2xl">{project.year}</p>
+          </div>
         </div>
+        <div className="p-8 ">
+          <h1 className="text-gray-500 text-2xl font-extrabold">DESCRIPTION</h1>
+          <p className="text-white text-xl text-justify">{project.full_description}</p>
+        </div>
+      </div>
+      <div className="flex flex-col items-center">
+        {project.full_image.map((image, idx) => {
+          return (
+            <div key={idx} className="w-1/2 md:w-1/2 sm:w-1/3 h-auto my-4 overflow-hidden">
+              <img src={image} alt={`Image ${idx}`} className="w-full border-4 border-gray-400 h-full object-cover" />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,17 +1,41 @@
 import pinjam from "../assets/img/pinjam.png";
+import pinjam11 from "../assets/img/pinjam11.png";
+import pinjam12 from "../assets/img/pinjam12.png";
 import bps from "../assets/img/bps.png";
+import bps11 from "../assets/img/bps11.png";
+import bps12 from "../assets/img/bps12.png";
+import bps13 from "../assets/img/bps13.png";
 import siami from "../assets/img/siami.png";
+import siami11 from "../assets/img/siami11.png";
+import siami12 from "../assets/img/siami12.png";
+import siami13 from "../assets/img/siami13.png";
+import siami14 from "../assets/img/siami14.png";
 import bc from "../assets/img/bc.png";
+import bc11 from "../assets/img/bc11.png";
+import bc12 from "../assets/img/bc12.png";
+import bc13 from "../assets/img/bc13.png";
 import lpg from "../assets/img/lpg.png";
+import lpg11 from "../assets/img/lpg11.png";
+import lpg12 from "../assets/img/lpg12.png";
 import pm from "../assets/img/pm.png";
+import pm11 from "../assets/img/pm11.png";
+import pm12 from "../assets/img/pm12.png";
+import pm13 from "../assets/img/pm13.png";
+import pm14 from "../assets/img/pm14.jpg";
 import am from "../assets/img/am.png";
+import am11 from "../assets/img/am11.png";
+import am12 from "../assets/img/am12.png";
+import am13 from "../assets/img/am13.png";
 export const projects = [
   {
     id: 1,
     title: "Customer LPG Pertamina",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    full_description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis vero aspernatur eveniet mollitia minus rerum dolor, voluptatum commodi iure soluta libero, explicabo atque animi fugiat dolorem dolorum! Fugit ducimus porro culpa libero nisi inventore harum nihil odit perferendis commodi, qui aperiam maxime eaque quae modi quia deserunt non, iure iste illo accusantium voluptatibus exercitationem. Vitae ab et cumque debitis voluptatem fuga nesciunt aperiam optio asperiores saepe a hic, esse sit.",
     technologies: ["Laravel", "MySQL", "Bootstrap", "Javascript", "jQuery", "AJAX"],
     image: lpg,
+    full_image: [lpg11, lpg12],
     category: "Web Development",
     year: 2024,
   },
@@ -19,8 +43,11 @@ export const projects = [
     id: 2,
     title: "Brata Cerdas",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    full_description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis vero aspernatur eveniet mollitia minus rerum dolor, voluptatum commodi iure soluta libero, explicabo atque animi fugiat dolorem dolorum! Fugit ducimus porro culpa libero nisi inventore harum nihil odit perferendis commodi, qui aperiam maxime eaque quae modi quia deserunt non, iure iste illo accusantium voluptatibus exercitationem. Vitae ab et cumque debitis voluptatem fuga nesciunt aperiam optio asperiores saepe a hic, esse sit.",
     technologies: ["Laravel", "MySQL", "Bootstrap", "Javascript", "jQuery", "AJAX", "Google OAuth", "Google Analytics"],
     image: bc,
+    full_image: [bc11, bc12, bc13],
     category: "Web Development",
     year: 2024,
   },
@@ -28,8 +55,11 @@ export const projects = [
     id: 3,
     title: "Asset Management",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    full_description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis vero aspernatur eveniet mollitia minus rerum dolor, voluptatum commodi iure soluta libero, explicabo atque animi fugiat dolorem dolorum! Fugit ducimus porro culpa libero nisi inventore harum nihil odit perferendis commodi, qui aperiam maxime eaque quae modi quia deserunt non, iure iste illo accusantium voluptatibus exercitationem. Vitae ab et cumque debitis voluptatem fuga nesciunt aperiam optio asperiores saepe a hic, esse sit.",
     technologies: ["Laravel", "MySQL", "Bootstrap", "Javascript", "jQuery", "AJAX"],
     image: am,
+    full_image: [am11, am12, am13],
     category: "Web Development",
     year: 2024,
   },
@@ -37,8 +67,11 @@ export const projects = [
     id: 4,
     title: "SIAMI",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    full_description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis vero aspernatur eveniet mollitia minus rerum dolor, voluptatum commodi iure soluta libero, explicabo atque animi fugiat dolorem dolorum! Fugit ducimus porro culpa libero nisi inventore harum nihil odit perferendis commodi, qui aperiam maxime eaque quae modi quia deserunt non, iure iste illo accusantium voluptatibus exercitationem. Vitae ab et cumque debitis voluptatem fuga nesciunt aperiam optio asperiores saepe a hic, esse sit.",
     technologies: ["Laravel", "MySQL", "Bootstrap", "Javascript", "jQuery", "Google OAuth"],
     image: siami,
+    full_image: [siami11, siami12, siami13, siami14],
     category: "Web Development",
     year: 2024,
   },
@@ -46,8 +79,11 @@ export const projects = [
     id: 5,
     title: "Monitoring DC",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    full_description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis vero aspernatur eveniet mollitia minus rerum dolor, voluptatum commodi iure soluta libero, explicabo atque animi fugiat dolorem dolorum! Fugit ducimus porro culpa libero nisi inventore harum nihil odit perferendis commodi, qui aperiam maxime eaque quae modi quia deserunt non, iure iste illo accusantium voluptatibus exercitationem. Vitae ab et cumque debitis voluptatem fuga nesciunt aperiam optio asperiores saepe a hic, esse sit.",
     technologies: ["Codeigniter", "MySQL", "Bootstrap", "Javascript", "jQuery", "Google OAuth", "SSO"],
     image: bps,
+    full_image: [bps11, bps12, bps13],
     category: "Web Development",
     year: 2023,
   },
@@ -55,8 +91,11 @@ export const projects = [
     id: 6,
     title: "Peminjaman Ruang",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    full_description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis vero aspernatur eveniet mollitia minus rerum dolor, voluptatum commodi iure soluta libero, explicabo atque animi fugiat dolorem dolorum! Fugit ducimus porro culpa libero nisi inventore harum nihil odit perferendis commodi, qui aperiam maxime eaque quae modi quia deserunt non, iure iste illo accusantium voluptatibus exercitationem. Vitae ab et cumque debitis voluptatem fuga nesciunt aperiam optio asperiores saepe a hic, esse sit.",
     technologies: ["Laravel", "MySQL", "Bootstrap", "Javascript", "jQuery"],
     image: pinjam,
+    full_image: [pinjam11, pinjam12],
     category: "Web Development",
     year: 2023,
   },
@@ -64,8 +103,11 @@ export const projects = [
     id: 7,
     title: "Predictive Maintenance",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    full_description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis vero aspernatur eveniet mollitia minus rerum dolor, voluptatum commodi iure soluta libero, explicabo atque animi fugiat dolorem dolorum! Fugit ducimus porro culpa libero nisi inventore harum nihil odit perferendis commodi, qui aperiam maxime eaque quae modi quia deserunt non, iure iste illo accusantium voluptatibus exercitationem. Vitae ab et cumque debitis voluptatem fuga nesciunt aperiam optio asperiores saepe a hic, esse sit.",
     technologies: ["Data Preprocessing", "Data Training", "Undersampled Data", " Optuna Optimization", "SVM", "KNN", "Random Forest", "Roc Curves"],
     image: pm,
+    full_image: [pm11, pm12, pm13, pm14],
     category: "Other",
     year: 2023,
   },
