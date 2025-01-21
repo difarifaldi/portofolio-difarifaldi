3a57d26dfb4c51ff078c96ec9a323034522fcbcf
difarifaldi <difarifaldi0906@gmail.com>
2025-01-21 18:00:58 +0700

    tambah logo

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import b3 from "../assets/img/dlogo.png"; // Pastikan path logo benar
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -26,16 +27,15 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className={`fixed w-full top-0 left-0 z-50 ${isScrolled ? "bg-transparent  backdrop-blur-lg" : "bg-gray-800"}`}>
+    <nav className={`fixed w-full top-0 left-0 z-50 ${isScrolled ? "bg-transparent backdrop-blur-lg" : "bg-gray-800"}`}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center">
+            {/* Logo dan Teks */}
+            <img src={b3} alt="Logo" className="w-8 h-8 mr-2" /> {/* Logo */}
             {/* Tampilkan tombol navigasi jika bukan di halaman utama */}
             {!isHome && (
-              <button
-                onClick={() => navigate(isProjectDetail ? "/detail-project" : "/")} // Navigasi berdasarkan kondisi
-                className="text-white font-bold text-xl flex items-center space-x-2"
-              >
+              <button onClick={() => navigate(isProjectDetail ? "/detail-project" : "/")} className="text-white font-bold text-xl flex items-center space-x-2">
                 <span className="text-xs">
                   <i className="fa fa-chevron-left"></i>
                 </span>
