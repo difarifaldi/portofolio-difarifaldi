ecdf2060f6587557871c246090f2165f817b4e6d
difarifaldi <difarifaldi0906@gmail.com>
2025-05-07 17:35:55 +0700

    perbaiki map

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
           </div>
         </motion.div>
         {/* Bagian Gambar */}
-        <motion.div className="w-1/2 md:w-1/2 sm:w-1/3 h-auto border-4 border-gray-300 overflow-hidden" initial={{ opacity: 0, x: 100 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 1 }}>
+        <motion.div className="w-full md:w-1/2  h-auto border-4 border-gray-300 overflow-hidden" initial={{ opacity: 0, x: 100 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 1 }}>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.9858334031696!2d106.84955656573081!3d-6.395826708673846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69eb8d54641b17%3A0x8f8ce60052fd1a6a!2sMerdeka%20Motor%20Depok!5e0!3m2!1sid!2sid!4v1736497090223!5m2!1sid!2sid"
             width="600"
